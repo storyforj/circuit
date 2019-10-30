dcad403da7ef88f9529df96c963d6093dbbe9a06
Parris Khachi <parris@storyforj.com>
2019-10-29 18:21:02 -0700

    Export typings, add in preact for the example app

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,40 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
-import typescript from 'rollup-plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
 
-export default {
-	input: 'src/example.ts',
-	output: {
-		file: 'public/bundle.js',
-		format: 'iife', // immediately-invoked function expression — suitable for <script> tags
-		sourcemap: true
+export default [
+	{
+		input: 'src/example.tsx',
+		output: {
+			file: 'public/bundle.js',
+			format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+			sourcemap: true
+		},
+		plugins: [
+			typescript(),
+			resolve(), // tells Rollup how to find date-fns in node_modules
+			commonjs(), // converts date-fns to ES modules
+			production && terser(), // minify, but only in production
+		]
 	},
-	plugins: [
-		typescript({lib: ["es5", "es6", "dom"], target: "es5" }),
-		resolve(), // tells Rollup how to find date-fns in node_modules
-		commonjs(), // converts date-fns to ES modules
-		production && terser() // minify, but only in production
-	]
-};
+	{
+		input: 'src/lib.ts',
+		output: {
+			name: 'circuit',
+			file: 'build/lib.js',
+			format: 'umd',
+			sourcemap: true
+		},
+		plugins: [
+			typescript(),
+			resolve(), // tells Rollup how to find date-fns in node_modules
+			commonjs(), // converts date-fns to ES modules
+			production && terser(), // minify, but only in production
+		]
+	},
+];
